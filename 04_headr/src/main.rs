63d8cb94d60ce5b0f57b638e0d9a19a97c0c3493
theharshpat <harsh.patel.magic105@gmail.com>
2026-06-14 19:28:05 +0530

    ch4 wip

--- 04_headr/src/main.rs
+++ 04_headr/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
 use anyhow::Result;
 use clap::Parser;
 
@@ -7,15 +12,28 @@ struct Args {
     #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
 
-    #[arg(short = 'n', long, default_value_t = 10)]
-    lines: usize,
+    #[arg(short = 'n', long, default_value_t = 10, value_parser = clap::value_parser!(u64).range(1..))]
+    lines: u64,
 
-    #[arg(short = 'c', long, conflicts_with = "lines")]
+    #[arg(short = 'c', long, conflicts_with = "lines", value_parser = clap::value_parser!(u64).range(1..))]
     bytes: Option<u64>,
 }
 
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 fn run(args: Args) -> Result<()> {
-    println!("Args: {:#?}", args);
+    for filename in args.files {
+        let reader_result = open(&filename);
+        match reader_result {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
 
