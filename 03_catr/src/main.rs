42f930afc71c410a2e7f9f2eea833857db4b6df4
theharshpat <harsh.patel.magic105@gmail.com>
2026-06-14 17:15:06 +0530

    ch3 add stdin support, use bufread, fixes

--- 03_catr/src/main.rs
+++ 03_catr/src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
-use std::fs;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -17,26 +20,41 @@ struct Args {
 
 fn run(args: Args) -> Result<()> {
     for file in &args.files {
+        let reader: Box<dyn BufRead> = if file == "-" {
+            Box::new(BufReader::new(io::stdin()))
+        } else {
+            Box::new(BufReader::new(File::open(file)?))
+        };
+
         let mut non_blank_line_count_so_far = 0;
-        for (idx, line) in fs::read_to_string(file)?.lines().enumerate() {
-            if &args.numbers == &true {
-                println!("{:6}\t{}", idx + 1, line);
+
+        for (idx, line) in reader.lines().enumerate() {
+            let line = line?;
+
+            if args.numbers {
+                println!("{:>6}\t{}", idx + 1, line);
                 continue;
             }
-            if &args.number_nonblank == &true {
-                if line.trim().is_empty() {
-                    println!("{}", line);
+
+            if args.number_nonblank {
+                if line.is_empty() {
+                    println!();
                 } else {
                     non_blank_line_count_so_far += 1;
-                    println!("{:6}\t{}", non_blank_line_count_so_far, line);
+
+                    println!("{:>6}\t{}", non_blank_line_count_so_far, line);
                 }
+
                 continue;
             }
-            println!("{}", line);
+
+            println!("{line}");
         }
     }
+
     Ok(())
 }
+
 fn main() {
     if let Err(e) = run(Args::parse()) {
         eprintln!("Error: {}", e);
