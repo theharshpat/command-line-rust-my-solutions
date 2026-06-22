b1c575152d72a7a07a8e7e36f537ae2fc090a0e1
theharshpat <harsh.patel.magic105@gmail.com>
2026-06-23 02:49:03 +0530

    ch 6 wip

--- 06_uniqr/src/main.rs
+++ 06_uniqr/src/main.rs
@@ -1,3 +1,46 @@
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug, Parser)]
+#[command(version)]
+/// Rust version of `uniq`
+struct Args {
+    /// Input file
+    #[arg(value_name = "IN_FILE", default_value = "-")]
+    in_file: String,
+
+    /// Output file
+    #[arg(value_name = "OUT_FILE")]
+    out_file: Option<String>,
+
+    /// Show counts
+    #[arg(short, long)]
+    count: bool,
+}
+
+fn run(args: Args) -> Result<()> {
+    let mut reader = open(&args.in_file)?;
+    let mut buffer = String::new();
+    while reader.read_line(&mut buffer)? > 0 {
+        // just print as it is
+        println!("{}", buffer);
+        buffer.clear();
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("Error: {e}");
+        std::process::exit(1);
+    }
 }
