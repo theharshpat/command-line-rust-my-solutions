3c09c999ecdda03dc8f065ffa0fea6a508fbf354
theharshpat <harsh.patel.magic105@gmail.com>
2026-06-23 20:11:14 +0530

    ch7 tests pass

--- 07_findr/src/main.rs
+++ 07_findr/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::{Parser, ValueEnum};
 use regex::Regex;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, Parser)]
 #[command(version, about = "Rust version of `find`")]
@@ -29,7 +30,39 @@ enum EntryType {
 }
 
 fn run(args: Args) -> Result<()> {
-    println!("Args {:#?}", args);
+    let type_filter = |entry: &DirEntry| {
+        args.entry_types.is_empty()
+            || args.entry_types.iter().any(|entry_type| match entry_type {
+                EntryType::Link => entry.file_type().is_symlink(),
+                EntryType::Dir => entry.file_type().is_dir(),
+                EntryType::File => entry.file_type().is_file(),
+            })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        args.names.is_empty()
+            || args
+                .names
+                .iter()
+                .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+    };
+
+    for path in &args.paths {
+        for entry in WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{e}");
+                    None
+                }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+        {
+            println!("{}", entry.path().display());
+        }
+    }
     Ok(())
 }
 
