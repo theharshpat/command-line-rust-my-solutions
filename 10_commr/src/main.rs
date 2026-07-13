5d1894c005baaf3a0a678110ed3dbad4adf0e51d
theharshpat <harsh.patel.magic105@gmail.com>
2026-07-13 10:06:50 +0530

    wip

--- 10_commr/src/main.rs
+++ 10_commr/src/main.rs
@@ -1,3 +1,63 @@
+use anyhow::{Result, anyhow, bail};
+use clap::{ArgAction, Parser};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug, Parser)]
+#[command(version)]
+/// Rust version of `comm`
+struct Args {
+    /// Input file 1
+    file1: String,
+
+    /// Input file 2
+    file2: String,
+
+    /// Suppress printing of column 1
+    #[arg(short('1'), action(ArgAction::SetFalse))]
+    show_col1: bool,
+
+    /// Suppress printing of column 2
+    #[arg(short('2'), action(ArgAction::SetFalse))]
+    show_col2: bool,
+
+    /// Suppress printing of column 3
+    #[arg(short('3'), action(ArgAction::SetFalse))]
+    show_col3: bool,
+
+    /// Case-insensitive comparison of lines
+    #[arg(short)]
+    insensitive: bool,
+
+    /// Output delimiter
+    #[arg(short, long = "output-delimiter", default_value = "\t")]
+    delimiter: String,
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(
+            File::open(filename).map_err(|e| anyhow!("{filename}: {e}"))?,
+        ))),
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    let file1 = &args.file1;
+    let file2 = &args.file2;
+    if file1 == "-" && file2 == "-" {
+        bail!(r#"Both input files cannot be STDIN ("-")"#);
+    }
+
+    let _fh1 = open(file1)?;
+    let _fh2 = open(file2)?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
