121a09861646033c70fea3d187fa4d798404860a
theharshpat <harsh.patel.magic105@gmail.com>
2026-07-15 13:31:32 +0530

    ch12 initial setup

--- 12_fortuner/src/main.rs
+++ 12_fortuner/src/main.rs
@@ -1,113 +1,143 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about = "Rust version of `fortune`")]
+pub struct Args {
+    /// Input files or directories
+    #[arg(required = true, value_name = "FILE")]
+    sources: Vec<String>,
+
+    /// Pattern
+    #[arg(short = 'm', long = "pattern", value_name = "PATTERN")]
+    pattern: Option<String>,
+
+    /// Case-insensitive pattern matching
+    #[arg(short, long)]
+    insensitive: bool,
+
+    /// Random seed
+    #[arg(short, long, value_name = "SEED")]
+    seed: Option<u64>,
+}
+
+fn run(args: Args) -> Result<()> {
+    println!("{args:#?}");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{find_files, pick_fortune, read_fortunes, Fortune};
-    use std::path::PathBuf;
-
-    #[test]
-    fn test_find_files() {
-        // Verify that the function finds a file known to exist
-        let res = find_files(&["./tests/inputs/jokes".to_string()]);
-        assert!(res.is_ok());
-
-        let files = res.unwrap();
-        assert_eq!(files.len(), 1);
-        assert_eq!(
-            files.get(0).unwrap().to_string_lossy(),
-            "./tests/inputs/jokes"
-        );
-
-        // Fails to find a bad file
-        let res = find_files(&["/path/does/not/exist".to_string()]);
-        assert!(res.is_err());
-
-        // Finds all the input files, excludes ".dat"
-        let res = find_files(&["./tests/inputs".to_string()]);
-        assert!(res.is_ok());
-
-        // Check number and order of files
-        let files = res.unwrap();
-        assert_eq!(files.len(), 5);
-        let first = files.get(0).unwrap().display().to_string();
-        assert!(first.contains("ascii-art"));
-        let last = files.last().unwrap().display().to_string();
-        assert!(last.contains("quotes"));
-
-        // Test for multiple sources, path must be unique and sorted
-        let res = find_files(&[
-            "./tests/inputs/jokes".to_string(),
-            "./tests/inputs/ascii-art".to_string(),
-            "./tests/inputs/jokes".to_string(),
-        ]);
-        assert!(res.is_ok());
-        let files = res.unwrap();
-        assert_eq!(files.len(), 2);
-        if let Some(filename) = files.first().unwrap().file_name() {
-            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
-        }
-        if let Some(filename) = files.last().unwrap().file_name() {
-            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
-        }
-    }
-
-    #[test]
-    fn test_read_fortunes() {
-        // Parses all the fortunes without a filter
-        let res = read_fortunes(&[PathBuf::from("./tests/inputs/jokes")]);
-        assert!(res.is_ok());
-
-        if let Ok(fortunes) = res {
-            // Correct number and sorting
-            assert_eq!(fortunes.len(), 6);
-            assert_eq!(
-                fortunes.first().unwrap().text,
-                "Q. What do you call a head of lettuce in a shirt and tie?\n\
-                A. Collared greens."
-            );
-            assert_eq!(
-                fortunes.last().unwrap().text,
-                "Q: What do you call a deer wearing an eye patch?\n\
-                A: A bad idea (bad-eye deer)."
-            );
-        }
-
-        // Filters for matching text
-        let res = read_fortunes(&[
-            PathBuf::from("./tests/inputs/jokes"),
-            PathBuf::from("./tests/inputs/quotes"),
-        ]);
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().len(), 11);
-    }
-
-    #[test]
-    fn test_pick_fortune() {
-        // Create a slice of fortunes
-        let fortunes = &[
-            Fortune {
-                source: "fortunes".to_string(),
-                text: "You cannot achieve the impossible without \
-                      attempting the absurd."
-                    .to_string(),
-            },
-            Fortune {
-                source: "fortunes".to_string(),
-                text: "Assumption is the mother of all screw-ups."
-                    .to_string(),
-            },
-            Fortune {
-                source: "fortunes".to_string(),
-                text: "Neckties strangle clear thinking.".to_string(),
-            },
-        ];
-
-        // Pick a fortune with a seed
-        assert_eq!(
-            pick_fortune(fortunes, Some(1)).unwrap(),
-            "Neckties strangle clear thinking.".to_string()
-        );
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::{Fortune, find_files, pick_fortune, read_fortunes};
+//     use std::path::PathBuf;
+
+//     #[test]
+//     fn test_find_files() {
+//         // Verify that the function finds a file known to exist
+//         let res = find_files(&["./tests/inputs/jokes".to_string()]);
+//         assert!(res.is_ok());
+
+//         let files = res.unwrap();
+//         assert_eq!(files.len(), 1);
+//         assert_eq!(
+//             files.get(0).unwrap().to_string_lossy(),
+//             "./tests/inputs/jokes"
+//         );
+
+//         // Fails to find a bad file
+//         let res = find_files(&["/path/does/not/exist".to_string()]);
+//         assert!(res.is_err());
+
+//         // Finds all the input files, excludes ".dat"
+//         let res = find_files(&["./tests/inputs".to_string()]);
+//         assert!(res.is_ok());
+
+//         // Check number and order of files
+//         let files = res.unwrap();
+//         assert_eq!(files.len(), 5);
+//         let first = files.get(0).unwrap().display().to_string();
+//         assert!(first.contains("ascii-art"));
+//         let last = files.last().unwrap().display().to_string();
+//         assert!(last.contains("quotes"));
+
+//         // Test for multiple sources, path must be unique and sorted
+//         let res = find_files(&[
+//             "./tests/inputs/jokes".to_string(),
+//             "./tests/inputs/ascii-art".to_string(),
+//             "./tests/inputs/jokes".to_string(),
+//         ]);
+//         assert!(res.is_ok());
+//         let files = res.unwrap();
+//         assert_eq!(files.len(), 2);
+//         if let Some(filename) = files.first().unwrap().file_name() {
+//             assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+//         }
+//         if let Some(filename) = files.last().unwrap().file_name() {
+//             assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+//         }
+//     }
+
+//     #[test]
+//     fn test_read_fortunes() {
+//         // Parses all the fortunes without a filter
+//         let res = read_fortunes(&[PathBuf::from("./tests/inputs/jokes")]);
+//         assert!(res.is_ok());
+
+//         if let Ok(fortunes) = res {
+//             // Correct number and sorting
+//             assert_eq!(fortunes.len(), 6);
+//             assert_eq!(
+//                 fortunes.first().unwrap().text,
+//                 "Q. What do you call a head of lettuce in a shirt and tie?\n\
+//                 A. Collared greens."
+//             );
+//             assert_eq!(
+//                 fortunes.last().unwrap().text,
+//                 "Q: What do you call a deer wearing an eye patch?\n\
+//                 A: A bad idea (bad-eye deer)."
+//             );
+//         }
+
+//         // Filters for matching text
+//         let res = read_fortunes(&[
+//             PathBuf::from("./tests/inputs/jokes"),
+//             PathBuf::from("./tests/inputs/quotes"),
+//         ]);
+//         assert!(res.is_ok());
+//         assert_eq!(res.unwrap().len(), 11);
+//     }
+
+//     #[test]
+//     fn test_pick_fortune() {
+//         // Create a slice of fortunes
+//         let fortunes = &[
+//             Fortune {
+//                 source: "fortunes".to_string(),
+//                 text: "You cannot achieve the impossible without \
+//                       attempting the absurd."
+//                     .to_string(),
+//             },
+//             Fortune {
+//                 source: "fortunes".to_string(),
+//                 text: "Assumption is the mother of all screw-ups.".to_string(),
+//             },
+//             Fortune {
+//                 source: "fortunes".to_string(),
+//                 text: "Neckties strangle clear thinking.".to_string(),
+//             },
+//         ];
+
+//         // Pick a fortune with a seed
+//         assert_eq!(
+//             pick_fortune(fortunes, Some(1)).unwrap(),
+//             "Neckties strangle clear thinking.".to_string()
+//         );
+//     }
+// }
