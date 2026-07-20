dd70c20087c078e33353d257650057a3051cb93a
theharshpat <harsh.patel.magic105@gmail.com>
2026-07-21 02:35:12 +0530

    ch12 discover files

--- 12_fortuner/src/main.rs
+++ 12_fortuner/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 #[derive(Debug, Parser)]
 #[command(version, about = "Rust version of `fortune`")]
@@ -22,8 +24,39 @@ pub struct Args {
 }
 
 fn run(args: Args) -> Result<()> {
+    find_files(&args.sources)?;
     println!("{args:#?}");
     Ok(())
+}
+
+fn find_files(paths: &[String]) -> Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+
+    for path in paths {
+        let path = PathBuf::from(path);
+        let metadata = path
+            .metadata()
+            .map_err(|err| anyhow::anyhow!("{}: {err}", path.display()))?;
+
+        if metadata.is_file() {
+            if path.extension().is_none_or(|ext| ext != "dat") {
+                files.push(path);
+            }
+        } else if metadata.is_dir() {
+            for entry in WalkDir::new(path) {
+                let entry = entry?;
+                let path = entry.path();
+
+                if entry.file_type().is_file() && path.extension().is_none_or(|ext| ext != "dat") {
+                    files.push(path.to_path_buf());
+                }
+            }
+        }
+    }
+
+    files.sort();
+    files.dedup();
+    Ok(files)
 }
 
 fn main() {
@@ -33,56 +66,55 @@ fn main() {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::{Fortune, find_files, pick_fortune, read_fortunes};
-//     use std::path::PathBuf;
+#[cfg(test)]
+mod tests {
+    use super::find_files;
 
-//     #[test]
-//     fn test_find_files() {
-//         // Verify that the function finds a file known to exist
-//         let res = find_files(&["./tests/inputs/jokes".to_string()]);
-//         assert!(res.is_ok());
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
 
-//         let files = res.unwrap();
-//         assert_eq!(files.len(), 1);
-//         assert_eq!(
-//             files.get(0).unwrap().to_string_lossy(),
-//             "./tests/inputs/jokes"
-//         );
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.get(0).unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
 
-//         // Fails to find a bad file
-//         let res = find_files(&["/path/does/not/exist".to_string()]);
-//         assert!(res.is_err());
+        // Fails to find a bad file
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
 
-//         // Finds all the input files, excludes ".dat"
-//         let res = find_files(&["./tests/inputs".to_string()]);
-//         assert!(res.is_ok());
+        // Finds all the input files, excludes ".dat"
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
 
-//         // Check number and order of files
-//         let files = res.unwrap();
-//         assert_eq!(files.len(), 5);
-//         let first = files.get(0).unwrap().display().to_string();
-//         assert!(first.contains("ascii-art"));
-//         let last = files.last().unwrap().display().to_string();
-//         assert!(last.contains("quotes"));
+        // Check number and order of files
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.get(0).unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
 
-//         // Test for multiple sources, path must be unique and sorted
-//         let res = find_files(&[
-//             "./tests/inputs/jokes".to_string(),
-//             "./tests/inputs/ascii-art".to_string(),
-//             "./tests/inputs/jokes".to_string(),
-//         ]);
-//         assert!(res.is_ok());
-//         let files = res.unwrap();
-//         assert_eq!(files.len(), 2);
-//         if let Some(filename) = files.first().unwrap().file_name() {
-//             assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
-//         }
-//         if let Some(filename) = files.last().unwrap().file_name() {
-//             assert_eq!(filename.to_string_lossy(), "jokes".to_string())
-//         }
-//     }
+        // Test for multiple sources, path must be unique and sorted
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
+    }
 
 //     #[test]
 //     fn test_read_fortunes() {
@@ -140,4 +172,4 @@ fn main() {
 //             "Neckties strangle clear thinking.".to_string()
 //         );
 //     }
-// }
+}
